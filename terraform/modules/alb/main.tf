c4f2b7bdc5afa1b571be2c0f06704e0c894803be
Nikhil Yadav <ynikhildp@gmail.com>
2026-09-03 22:30:43 +0530

    fixing ALB setting

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -24,12 +24,15 @@ resource "aws_security_group" "alb" {
   }
 }
 
+#trivy:ignore:AWS-0053
+
 resource "aws_lb" "this" {
   name               = "${var.project_name}-${var.environment}-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb.id]
   subnets            = values(var.public_subnet_ids)
+  drop_invalid_header_fields = true
 
   enable_deletion_protection = var.enable_deletion_protection
 
@@ -62,6 +65,7 @@ resource "aws_lb_target_group" "this" {
   }
 }
 
+#trivy:ignore:AWS-0054
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
